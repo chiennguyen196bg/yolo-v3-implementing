c5fdb56da936b75e7fc233bd8405b2a06c9a3008
chiennm <chienminhnguyen196@gmail.com>
2018-11-02 00:51:30 +0700

    predict with batch return batch of result with max_boxes is configed

--- detect.py
+++ detect.py
@@ -13,11 +13,11 @@ def detect(image_path, class_names, model_path, yolo_weights=None):
     image_data = np.array(resize_image, dtype=np.float32)
     image_data /= 255.
     image_data = np.expand_dims(image_data, axis=0)
-    input_image_shape = tf.placeholder(dtype=tf.int32, shape=(2,))
+    input_image_shape = tf.placeholder(dtype=tf.int32, shape=(None, 2))
     input_image = tf.placeholder(shape=[None, 416, 416, 3], dtype=tf.float32)
     model = Yolov3(cfg.BATCH_NORM_DECAY, cfg.BATCH_NORM_EPSILON, cfg.LEAKY_RELU, cfg.ANCHORS, len(class_names))
     yolo_outputs = model.yolo_inference(input_image, is_training=False)
-    boxes, scores, classes = model.yolo_predict(yolo_outputs, input_image_shape)
+    result_bbox = model.yolo_predict(yolo_outputs, input_image_shape)
     with tf.Session() as sess:
         # sess.run(tf.global_variables_initializer())
         if yolo_weights is not None:
@@ -26,20 +26,25 @@ def detect(image_path, class_names, model_path, yolo_weights=None):
         else:
             saver = tf.train.Saver()
             saver.restore(sess, model_path)
-        out_boxes, out_scores, out_classes = sess.run(
-            [boxes, scores, classes],
+        out_result_bbox = sess.run(
+            [result_bbox],
             feed_dict={
                 input_image: image_data,
-                input_image_shape: [image.size[1], image.size[0]]
+                input_image_shape: [[image.size[1], image.size[0]]]
             })
-        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+
+        out_result_bbox = out_result_bbox[0][0]
+        out_result_bbox = out_result_bbox[out_result_bbox[:, 4] > 0]
+
+        print('Found {} boxes for {}'.format(len(out_result_bbox[0]), 'img'))
         font = None
         thickness = (image.size[0] + image.size[1]) // 300
 
-        for i, c in reversed(list(enumerate(out_classes))):
+        for i, c in reversed(list(enumerate(out_result_bbox[:, 5]))):
+            c = int(c)
             predicted_class = class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
+            box = out_result_bbox[i, 0:4]
+            score = out_result_bbox[i, 4]
 
             label = '{} {:.2f}'.format(predicted_class, score)
             draw = ImageDraw.Draw(image)
@@ -65,7 +70,7 @@ def detect(image_path, class_names, model_path, yolo_weights=None):
             draw.rectangle(
                 [tuple(text_origin), tuple(text_origin + label_size)],
                 fill=(66, 134, 244))
-            draw.text((left, top), label, fill=(0, 0, 0), font=font)
+            draw.text(tuple(text_origin), label, fill=(0, 0, 0), font=font)
             del draw
         image.show()
         image.save('./result1.jpg')
@@ -74,9 +79,9 @@ def detect(image_path, class_names, model_path, yolo_weights=None):
 # (66, 134, 244)
 
 if __name__ == '__main__':
-    # with open('./model-data/coco_classes.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     class_names = list(x.strip() for x in lines)
+    with open('./model-data/coco_classes.txt', 'r') as f:
+        lines = f.readlines()
+        class_names = list(x.strip() for x in lines)
     # print(len(class_names))
-    # detect('./testimage.jpg', class_names, model_path=None, yolo_weights='./model-data/yolov3.weights')
-    detect('./NFPA-Labels.jpg', ['nfpa'], model_path='./checkpoint/model.ckpt-270', yolo_weights=None)
+    detect('./800px-People_at_Confluence_Park-2.jpg', class_names, model_path=None, yolo_weights='./model-data/yolov3.weights')
+    # detect('./NFPA-Labels.jpg', ['nfpa'], model_path='./checkpoint/model.ckpt-270', yolo_weights=None)

a533d98cc22ff51499a88d07fc9599a273f6f576
chiennguyen196-ubuntu <chienminhnguyen196@gmail.com>
2018-11-06 16:01:54 +0700

    add function initialize_uninitialized

--- train.py
+++ train.py
@@ -70,6 +70,7 @@ def train():
         if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
             print('Restore model', ckpt.model_checkpoint_path)
             saver.restore(sess, ckpt.model_checkpoint_path)
+            initialize_uninitialized(sess)
         else:
             print('Did not find a checkpoint. Initialize weights')
             sess.run(init_variables)
@@ -117,33 +118,44 @@ def train():
             )
 
             # Calculate mAP
-            #if epoch % 10 == 0:
-             #   predict_ = []
-              #  grouth_truth_ = []
-               # sess.run(test_init)
-               # try:
-                #    while True:
-                 #       out_predict_box, out_bbox = sess.run([predict_box, bbox], {is_training: False})
-                  #      predict_.append(out_predict_box)
-                   #     grouth_truth_.append(out_bbox)
-                #except tf.errors.OutOfRangeError:
-                #    pass
-                #grouth_truth_ = np.concatenate(grouth_truth_, axis=0)
-                #predict_ = np.concatenate(predict_, axis=0)
-                #AP = cal_AP(predict_, grouth_truth_, cfg.NUM_CLASSES, 0.5)
-                #mAP = np.mean(AP)
-                #test_writer.add_summary(
-                #    summary=tf.Summary(value=[tf.Summary.Value(tag='mAP', simple_value=mAP)]),
-                #    global_step=global_step_value
-                #)
-                #del grouth_truth_
-                #del predict_
-                #del AP
-                #del mAP
+            if epoch % 10 == 0:
+                print("Calculate mAP on test set")
+                predict_ = []
+                grouth_truth_ = []
+                sess.run(test_init)
+                try:
+                    while True:
+                        out_predict_box, out_bbox = sess.run([predict_box, bbox], {is_training: False})
+                        predict_.append(out_predict_box)
+                        grouth_truth_.append(out_bbox)
+                except tf.errors.OutOfRangeError:
+                    pass
+                grouth_truth_ = np.concatenate(grouth_truth_, axis=0)
+                predict_ = np.concatenate(predict_, axis=0)
+                AP = cal_AP(predict_, grouth_truth_, cfg.NUM_CLASSES, 0.5)
+                mAP = np.mean(AP)
+                test_writer.add_summary(
+                    summary=tf.Summary(value=[tf.Summary.Value(tag='mAP', simple_value=mAP)]),
+                    global_step=global_step_value
+                )
+                del grouth_truth_
+                del predict_
+                del AP
+                del mAP
 
             if epoch % 5 == 0:
                 checkpoint_path = os.path.join(cfg.CHECKPOINT_DIR, 'model.ckpt')
                 saver.save(sess, checkpoint_path, global_step=epoch)
+
+
+def initialize_uninitialized(sess):
+    global_vars = tf.global_variables()
+    is_not_initialized = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+    not_initialized_vars = [v for (v, f) in zip(global_vars, is_not_initialized) if not f]
+
+    # print([str(i.name) for i in not_initialized_vars])  # only for testing
+    if len(not_initialized_vars):
+        sess.run(tf.variables_initializer(not_initialized_vars))
 
 
 if __name__ == '__main__':

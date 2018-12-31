49437c5ea9449c1bd1f4bfd2232dd79439631007
chiennm <chienminhnguyen196@gmail.com>
2019-01-01 01:29:25 +0700

    change config for KITTI dataset

--- yolov3/config.py
+++ yolov3/config.py
@@ -3,11 +3,10 @@ BATCH_NORM_DECAY = 0.9
 BATCH_NORM_EPSILON = 1e-05
 LEAKY_RELU = 0.1
 
-# ANCHORS = [(5, 13), (8, 23), (20, 17), (12, 38), (33, 21), (19, 54), (41, 29), (36, 98), (72, 194)]
 ANCHORS = [(4, 11), (5, 15), (10, 11), (7, 21), (10, 24), (11, 36), (16, 45), (22, 59), (47, 137)]
 
-INPUT_SHAPE = [416, 320]
-NUM_CLASSES = 1
+INPUT_SHAPE = [416, 416]
+NUM_CLASSES = 4
 
 DATASET_DIR = "dataset/MOT17Det"
 
